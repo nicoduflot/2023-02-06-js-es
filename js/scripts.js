bb5a0cf3c6e63c1f6b7a99da60a16dc4d33d1b60
unknown <nduflot@dawan.fr>
2023-02-08 12:56:15 +0100

    cookie

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/* fonction de création et destruction d'un cookie */
+
+function setCookie(name, value = '', days = -1, path = '/'){
+    let maxAge = days * 25 * 60 * 60;
+    document.cookie = `${name}=${value}; max-age=${maxAge}; path=${path}; Samesite=Strict; Secure`;
+}
+
+/* fonction pour lire la valeur d'un cookie */
+
+function getCookie(name){
+    let tabCookie = document.cookie.split('; ');
+    console.log(tabCookie);
+    for(cookie of tabCookie){
+        console.log(cookie);
+        let tabValue = cookie.split('=');
+        console.log(tabValue);
+        if(tabValue[0] === name){
+            return tabValue[1];
+        }
+    }
+    return false;
+}
